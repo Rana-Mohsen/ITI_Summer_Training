aff5bbf7debd388fb2af75cffcaf7e1cb31c7a5c
Rana-3r2m <rana3r2m@gmail.com>
2023-08-17 09:40:06 +0300

    firebase database

--- lib/view/screens/screen3.dart
+++ lib/view/screens/screen3.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Screen3 extends StatelessWidget {
+class Screen3 extends StatefulWidget {
   const Screen3({super.key});
 
   @override
+  State<Screen3> createState() => _Screen3State();
+}
+
+class _Screen3State extends State<Screen3> {
+  final Stream<QuerySnapshot> _dayStream =
+      FirebaseFirestore.instance.collection('day').snapshots();
+
+  @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text("screen3"),
-    );;
+    return StreamBuilder<QuerySnapshot>(
+      stream: _dayStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              leading: Text(document.id),
+              title: Text(data['lesson']),
+              subtitle: Text(data['duration']),
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 }
